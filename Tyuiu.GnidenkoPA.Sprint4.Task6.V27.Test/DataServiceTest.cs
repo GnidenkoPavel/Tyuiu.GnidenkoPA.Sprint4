e8aeb2af56bf592970eb712a5ad9b46b688e23ba
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 03:31:38 +0500

    Task6

---
+++ Tyuiu.GnidenkoPA.Sprint4.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GnidenkoPA.Sprint4.Task6.V27.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint4.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            var shapes = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+            int res = ds.Calculate(shapes);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint4.Task6.V27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint4.Task6.V27.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint4.Task6.V27
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();   
+
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан строковый массив данных.                                            *");
+        Console.WriteLine("* Используя класс Array, подсчитайте количество элементов,                *");
+        Console.WriteLine("* длина которых меньше 7                                                  *");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        var shapes = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < shapes.Length; i++)
+        {
+            Console.WriteLine(shapes[i]);
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Количество элементов, длина которых меньше 7: ");
+
+        int nums = ds.Calculate(shapes);
+
+        Console.WriteLine(nums);
+        Console.ReadKey();
+    }
+}
+}

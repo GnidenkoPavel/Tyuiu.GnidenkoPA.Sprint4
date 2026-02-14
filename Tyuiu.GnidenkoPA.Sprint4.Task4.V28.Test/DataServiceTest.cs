cb0fc25d2561f4620ad9e980b970aecfc89eb896
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 02:50:23 +0500

    Task4

---
+++ Tyuiu.GnidenkoPA.Sprint4.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.GnidenkoPA.Sprint4.Task4.V28.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint4.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mtrx = new int[5, 5] { { 3, 3, 1, 1, 4 },
+                                          { 2, 4, 2, 1, 5 },
+                                          { 2, 2, 2, 2, 2 },
+                                          { 1, 5, 4, 5, 4 },
+                                          { 1, 2, 3, 2, 4 } };
+
+            int[,] res = ds.Calculate(mtrx);
+
+            int[,] wait = new int[5, 5] { { 3, 3, 1, 1, 1 },
+                                          { 1, 1, 1, 1, 5 },
+                                          { 1, 1, 1, 1, 1 },
+                                          { 1, 5, 1, 5, 1 },
+                                          { 1, 1, 3, 1, 1 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint4.Task4.V28/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint4.Task4.V28.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint4.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный      *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 5. Заменить четные       *");
+            Console.WriteLine("* элементы массива на 1.                                                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("**************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            int[,] res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Массив с замененными четными элементами:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}

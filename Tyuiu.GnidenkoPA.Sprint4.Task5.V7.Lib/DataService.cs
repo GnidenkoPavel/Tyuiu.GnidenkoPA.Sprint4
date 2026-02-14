94e8fa07ee39c2362c0521fee527864e88d86562
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 03:09:50 +0500

    Task5

---
+++ Tyuiu.GnidenkoPA.Sprint4.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.GnidenkoPA.Sprint4.Task5.V7.Lib
+{
+    public class DataService : ISprint4Task5V7
+    {
+        public int Calculate(int[,] matrix)
+        {
+            
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
